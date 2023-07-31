6e8e31098a91a0558eb51d211fbeda1d79928a1b
Boluwatife2904 <sanvicola2000@gmail.com>
2023-07-31 11:51:16 +0100

    chore: debugging in prod

--- plugins/theme.client.ts
+++ plugins/theme.client.ts
@@ -2,9 +2,11 @@ import useStatefulCookie from "~/composables/useStatefulCookie";
 
 export default defineNuxtPlugin(() => {
 	const theme = useStatefulCookie("theme") ?? "light-mode";
+	console.log("theme >>>>", theme);
 	const { setTheme } = useTheme();
 	const hasDarkPreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
+	console.log("has dark preference >>>>", hasDarkPreference);
+	console.log("typeof theme >>>>>", typeof theme);
 	if (typeof theme !== "undefined") {
 		setTheme(theme.value as string);
 	} else {
