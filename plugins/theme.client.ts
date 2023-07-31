4c8bd1c2af9c3e91864bfaa8e0b104cc2ae9af49
Boluwatife2904 <sanvicola2000@gmail.com>
2023-07-31 11:55:23 +0100

    chore: try fix for theme toggler

--- plugins/theme.client.ts
+++ plugins/theme.client.ts
@@ -1,13 +1,11 @@
 import useStatefulCookie from "~/composables/useStatefulCookie";
 
 export default defineNuxtPlugin(() => {
-	const theme = useStatefulCookie("theme") ?? "light-mode";
-	console.log("theme >>>>", theme);
+	const theme = useStatefulCookie("theme");
 	const { setTheme } = useTheme();
 	const hasDarkPreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-	console.log("has dark preference >>>>", hasDarkPreference);
-	console.log("typeof theme >>>>>", typeof theme);
-	if (typeof theme !== "undefined") {
+
+	if (theme.value) {
 		setTheme(theme.value as string);
 	} else {
 		setTheme(hasDarkPreference ? "dark-mode" : "light-mode");
